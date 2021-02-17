dab1c150121d3435b6f54e2c5ae014c7cdaeab44
yaowenqiang <yaowenqiang111@163.com>
2021-02-17 16:30:42 +0800

    ad bitwise operation demo

--- NOTES.md
+++ NOTES.md
@@ -63,3 +63,38 @@ NaN * anything = NaN
 infinity*infinity = infinity
 
 
+> Never use floating point numbers as map keys 
+
+i> Don't compare floating points for equality
+Alternatives to Floating Point Equality
+ + Can you use integers
+ + Can you use comparison
+
+ // if (f1 == f2)
+ if (f1 >= f2)
++ Can you check for nearness in value?
+
+//if (f1 == f2)
+epsilon = 1.0e-6
+if abs(f1-f2) < epsilon
+
+
+# Logical Operators
+
++ NOT
++ AND
++ OR
++ Exclusive OR
++ Inclusive OR
+
+> If John eats the ice cream or he eats the cake, there will only be one thing left for me to eat.
+
+
+> Truth table
+
+NOT (x > 3) AND (NOT found)  => NOT(x>3) OR found => x<=3 OR found
+
+Removing a Flag from a value
+
+RegexOptions = oldValue & (~RegexOptions.IgnoreCase0)
+
--- main.go
+++ main.go
@@ -2,6 +2,39 @@ package main
 
 import (
     "fmt"
+    "log"
+)
+
+/*
+const (
+    None = 0
+    EnSuite = 1
+    SeaView = 2
+    StairFreeAccess 4
+    Breakfast = 8
+    //etc
+)
+*/
+const (
+    //None = 0x0
+    //EnSuite = 0x1
+    //SeaView = 0x2
+    //StairFreeAccess = 0x4
+    //Breakfast = 0x8
+    //0x10
+    //0x20
+    //0x40
+    //0x80
+    //etc
+)
+
+const (
+    None = 0
+    EnSuite = 1
+    SeaView = 1 << 1
+    StairFreeAccess = 1 << 2
+    Breakfast = 1 << 3
+    //etc
 )
 
 func main() {
@@ -44,16 +77,82 @@ func main() {
     fmt.Printf("%+v\n", infinity*infinity)
 
 
-    //var float1, float2, float3, float4 float64
-    float1 := 1.06
-    float2 := 0.06
-    float3 := float1 + float2
-    float4 := 1.12
+    var float1, float2, float3, float4 float64
+    float1 = 1.07
+    float2 = 0.07
+    float3 = float1 + float2
+    float4 = 1.14
 
-    fmt.Printf("%.10f\n", float1)
-    fmt.Printf("%.10f\n", float2)
-    fmt.Printf("%.10f\n", float1 + float2)
-    fmt.Printf("%.10f\n", float3)
-    fmt.Printf("%.10f\n", float4)
+    fmt.Printf("%.20f\n", float1)
+    fmt.Printf("%.20f\n", float2)
+    fmt.Printf("%.20f\n", float1 + float2)
+    fmt.Printf("%.20f\n", float3)
+    fmt.Printf("%.20f\n", float4)
+    fmt.Printf("%+v\n", float3 == float4)
+
+
+    float5 := 0.25
+    float6 := 0.2
+    float7 := 0.05
+    float8 := float6 + float7
+
+    fmt.Printf("%.20f\n", float5)
+    fmt.Printf("%.20f\n", float8)
+
+    //true
+    fmt.Printf("%+v\n", float8 == float5)
+
+    float9 := 0.05
+    float10 := float9
+
+    //true
+    fmt.Printf("%+v\n", float9 == float10)
+
+    float11 := 0.05
+    float12 := float64(float32(float11))
+
+    fmt.Printf("%.20f\n", float11)
+    fmt.Printf("%.20f\n", float12)
+    //false
+    fmt.Printf("%+v\n", float11 == float12)
+
+
+    //Never use float point as a map key
+    map1 := make(map[float64]int)
+    map1[float11] = 1
+    map1[float12] = 1
+
+    fmt.Println(map1)
+    fmt.Println(log.Flags())
+    fmt.Println(log.Ldate)
+    fmt.Println(log.Ltime)
+    fmt.Println(log.Lmicroseconds)
+    fmt.Println(log.Llongfile)
+    fmt.Println(log.Lshortfile)
+    fmt.Println(log.LUTC)
+    fmt.Println(log.Lmsgprefix)
+    fmt.Println(log.LstdFlags)
+    log.Println("hello world")
+    //log.SetFlags(log.Llongfile|log.LUTC|log.Ltime)
+    //log.SetFlags(log.Flags() & (~log.Ldate))
+    //log.SetFlags(log.Flags() & (^log.Ldate))
+    if((log.Flags() & log.Ldate) != 0) {
+        log.Println("lDate flags is set")
+    } else {
+        log.Println("lDate flags isn't set")
+    }
+    log.SetFlags(log.Flags() & (^log.Ldate))
+    if((log.Flags() & log.Ldate) != 0) {
+        log.Println("lDate flags is set")
+    } else {
+        log.Println("lDate flags isn't set")
+    }
+    log.Println("long file")
+
+
+
+
+
+
 
 }
